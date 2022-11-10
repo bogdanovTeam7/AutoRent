3734fa909ac028bbda53b5c8459b9df43ebfabda
bogdanovTeam7 <bogdanov@t-email.hu>
2022-11-10 11:17:29 +0100

    init

---
+++ Tom-SB-MVC-Autorent/src/main/java/pti/sb_mvc_autorent/TomSbMvcAutorentApplication.java
@@ -0,0 +1,13 @@
+package pti.sb_mvc_autorent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TomSbMvcAutorentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TomSbMvcAutorentApplication.class, args);
+	}
+
+}
---
+++ Tom-SB-MVC-Autorent/src/test/java/pti/sb_mvc_autorent/TomSbMvcAutorentApplicationTests.java
@@ -0,0 +1,13 @@
+package pti.sb_mvc_autorent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TomSbMvcAutorentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
